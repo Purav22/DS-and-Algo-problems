1c170b970aebf82d31f90dadcf3754f545e5def4
Purav22 <ppp22112000@gmail.com>
2021-01-30 01:14:13 +0530

    updated

--- LinkedList/quetions.java
+++ LinkedList/quetions.java
@@ -1,7 +1,8 @@
 package LinkedList;
 
+import java.util.Stack;
 
- class ListNode {
+class ListNode {
          int val;
          ListNode next;
          ListNode() {}
@@ -43,7 +44,7 @@ public class quetions {
 
         while (curr != null) {
             ListNode forw = curr.next; // backup
-
+            //count++;
             curr.next = prev; // link
 
             prev = curr; // move
@@ -51,6 +52,63 @@ public class quetions {
         }
 
         return prev;
+    }
+    public static ListNode reverseList1(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode forw = curr.next; // backup
+            count++;
+            curr.next = prev; // link
+
+            prev = curr; // move
+            curr = forw;
+        }
+
+        return prev;
+    }
+    static int count = 0;
+    public static int[] nextLargerNodes(ListNode head) {
+        Stack<ListNode> st = new Stack<>();
+        ListNode h = reverseList1(head);
+        // ListNode temp = h;
+        // while(temp != null){
+        //     System.out.println(temp.val);
+        //     temp = temp.next;
+        // }
+        int ans[] = new int[count];
+        //System.out.println(count);
+        int i = ans.length - 1;
+        //System.out.println(h.val);
+        //System.out.println(i);
+        st.push(h);
+        ans[i] = 0;
+        i--;
+        h = h.next;
+        while(h != null){
+            //System.out.println(h.val);
+            while(st.size() > 0 && st.peek().val < h.val){
+                
+                st.pop();
+            }
+            
+            if(st.size() == 0){
+                ans[i] = 0;
+            }else{
+                ans[i] = st.peek().val;
+                //System.out.println(i + " " + ans[i]);
+            }
+            
+            st.push(h);
+            //if(st.size() > 0)System.out.println(st.peek().val);
+            i--;
+            h = h.next;
+        }
+        return ans;
     }
     public ListNode midNode(ListNode head)
     {
@@ -161,6 +219,66 @@ public class quetions {
         ListNode list2 = mergeKLists(lists, mid + 1, ei);
 
         return mergeTwoLists(list1, list2);
+        
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // ListNode dummy = new ListNode(0);
+        // dummy.next = head;
+        // int length  = 0;
+        // ListNode first = head;
+        // while (first != null) {
+        //     length++;
+        //     first = first.next;
+        // }
+        // length -= n;
+        // first = dummy;
+        // while (length > 0) {
+        //     length--;
+        //     first = first.next;
+        // }
+        // first.next = first.next.next;
+        // return dummy.next;
+        
+   
+        if (head == null)
+            return head;
+
+        ListNode c1 = head;
+        ListNode c2 = head;
+
+        while (n-- > 0)
+            c2 = c2.next;
+
+        if (c2 == null)
+        {
+            ListNode temp = head;
+            head = head.next;
+            temp.next = null;
+            return head;
+        }
+
+        while (c2.next != null)
+        {
+            c2 = c2.next;
+            c1 = c1.next;
+        }
+
+        ListNode temp = c1.next;
+        c1.next = c1.next.next;
+        temp.next = null;
+
+        return head;
+    
+    }
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode head = l1;
+        l1.next = new ListNode(1,null);
+        l1 = l1.next;
+        l1.next = new ListNode(5,null);
+        int ans[] = nextLargerNodes(head);
+        for(int num : ans)
+            System.out.println(num);
         
     }
     
